dbd75bbdce66d9544ad1fcaf63ed8685d170c0bc
Jean Rigotti <gmgotti@protonmail.com>
2018-07-04 16:38:38 +0200

    Update example to cover more features

--- examples/test.cjsx
+++ examples/test.cjsx
@@ -2,9 +2,21 @@ React = require('react')
 
 testConstant = 'foo'
 
+module.exports.named = 'named module exports'
+
 module.exports = React.createClass({
+  handleClick: ->
+    test = 'button'
+    other = 'yes'
+
+    if false
+      other = 'no'
+
+    console.log test
+
   render: ->
     <div className="hello">
       <div>Hello</div>
+      <button onClick={@handleClick}>Button</button>
     </div>
 })
--- examples/test.jsx
+++ examples/test.jsx
@@ -1,1 +1,27 @@
-opa
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+import React from 'react';
+
+const testConstant = 'foo';
+
+export const named = 'named module exports';
+
+export default class extends React.Component {
+  handleClick = () => {
+    const test = 'button';
+    let other = 'yes';
+
+    if (false) {
+      other = 'no';
+    }
+
+    return console.log(test);
+  };
+
+  render() {
+    return <div className="hello"><div>Hello</div><button onClick={this.handleClick}>Button</button></div>;
+  }
+}
