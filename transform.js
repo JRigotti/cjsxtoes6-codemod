705fccac97939c7aa214961617260611c710cbd2
Jean Rigotti <gmgotti@protonmail.com>
2018-07-04 16:25:25 +0200

    Add class properties

--- transform.js
+++ transform.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 const coffeeTransform = require('coffee-react-transform');
 const { convert: decaf } = require('decaffeinate');
+const classes = require('./node_modules/react-codemod/transforms/class.js');
 
 module.exports = (fileInfo, api, options) => {
     const newFilePath = path.format({
@@ -17,6 +18,8 @@ module.exports = (fileInfo, api, options) => {
     source = coffeeTransform(fileInfo.source);
     // convert from coffee to js
     source = decaf(source).code;
+    // use class properties
+    source = classes({ ...fileInfo, source }, api, options);
 
     const res = fs.writeFile(newFilePath, source, 'utf8', (err) => {
         if (err) {
