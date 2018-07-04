c24ca5b6b2798117435a36916d4da29887138f96
Jean Rigotti <gmgotti@protonmail.com>
2018-07-04 15:38:41 +0200

    Create new file based on input filename

--- transform.js
+++ transform.js
@@ -1,3 +1,16 @@
-module.exports = (fileInfo, api, options) => {
+const path = require('path');
+const fs = require('fs');
 
+module.exports = (fileInfo, api, options) => {
+    const newFilePath = path.format({
+        ext: '.jsx',
+        dir: path.dirname(fileInfo.path),
+        name: path.basename(fileInfo.path, path.extname(fileInfo.path))
+    });
+
+    const res = fs.writeFile(newFilePath, 'opa', 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 };
