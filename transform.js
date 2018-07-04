ee1d21b2f4db6eb4276e608f7025bdcb5b64faed
Jean Rigotti <gmgotti@protonmail.com>
2018-07-04 16:32:31 +0200

    Change export and require to import and export

--- transform.js
+++ transform.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const coffeeTransform = require('coffee-react-transform');
 const { convert: decaf } = require('decaffeinate');
 const classes = require('./node_modules/react-codemod/transforms/class.js');
+const toJSX = require('./node_modules/react-codemod/transforms/create-element-to-jsx.js');
+const exports = require('./node_modules/5to6-codemod/transforms/exports.js');
+const cjs = require('./node_modules/5to6-codemod/transforms/cjs.js');
 
 module.exports = (fileInfo, api, options) => {
     const newFilePath = path.format({
@@ -20,6 +23,12 @@ module.exports = (fileInfo, api, options) => {
     source = decaf(source).code;
     // use class properties
     source = classes({ ...fileInfo, source }, api, options);
+    // add jsx tags
+    source = toJSX({ ...fileInfo, source }, api, options);
+    // exports to import style
+    source = exports({ ...fileInfo, source }, api, options);
+    // use import instead of require
+    source = cjs({ ...fileInfo, source }, api, options);
 
     const res = fs.writeFile(newFilePath, source, 'utf8', (err) => {
         if (err) {
